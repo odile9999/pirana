fafc2c1f1f3f727eb720335594c454709c83f383
Odile@pc2050 <odile@pc2050.irap>
2015-03-27 19:22:46 +0100

    push test

--- src/gui/masstab_selector_qt.py
+++ src/gui/masstab_selector_qt.py
@@ -9,32 +9,25 @@
 
 from PyQt4 import QtCore, QtGui
 
-
 def _fromUtf8(s):
     return s
 
 try:
     _encoding = QtGui.QApplication.UnicodeUTF8
-
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig, _encoding)
 except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 
-
 class Ui_DockWidget_MassTabSelector(object):
-
     def setupUi(self, DockWidget_MassTabSelector):
-        DockWidget_MassTabSelector.setObjectName(
-            _fromUtf8("DockWidget_MassTabSelector"))
+        DockWidget_MassTabSelector.setObjectName(_fromUtf8("DockWidget_MassTabSelector"))
         DockWidget_MassTabSelector.resize(150, 500)
         DockWidget_MassTabSelector.setMinimumSize(QtCore.QSize(150, 300))
         DockWidget_MassTabSelector.setMaximumSize(QtCore.QSize(150, 700))
-        DockWidget_MassTabSelector.setFeatures(
-            QtGui.QDockWidget.DockWidgetFloatable | QtGui.QDockWidget.DockWidgetMovable)
-        DockWidget_MassTabSelector.setAllowedAreas(
-            QtCore.Qt.LeftDockWidgetArea | QtCore.Qt.RightDockWidgetArea)
+        DockWidget_MassTabSelector.setFeatures(QtGui.QDockWidget.DockWidgetFloatable|QtGui.QDockWidget.DockWidgetMovable)
+        DockWidget_MassTabSelector.setAllowedAreas(QtCore.Qt.LeftDockWidgetArea|QtCore.Qt.RightDockWidgetArea)
         self.dockWidgetContents = QtGui.QWidget()
         self.dockWidgetContents.setObjectName(_fromUtf8("dockWidgetContents"))
         self.gridLayout = QtGui.QGridLayout(self.dockWidgetContents)
@@ -43,8 +36,7 @@ class Ui_DockWidget_MassTabSelector(object):
         self.listView_Mass.setObjectName(_fromUtf8("listView_Mass"))
         self.gridLayout.addWidget(self.listView_Mass, 0, 0, 1, 1)
         self.pushButton_ChangeList = QtGui.QPushButton(self.dockWidgetContents)
-        self.pushButton_ChangeList.setObjectName(
-            _fromUtf8("pushButton_ChangeList"))
+        self.pushButton_ChangeList.setObjectName(_fromUtf8("pushButton_ChangeList"))
         self.gridLayout.addWidget(self.pushButton_ChangeList, 1, 0, 1, 1)
         DockWidget_MassTabSelector.setWidget(self.dockWidgetContents)
 
@@ -52,7 +44,6 @@ class Ui_DockWidget_MassTabSelector(object):
         QtCore.QMetaObject.connectSlotsByName(DockWidget_MassTabSelector)
 
     def retranslateUi(self, DockWidget_MassTabSelector):
-        DockWidget_MassTabSelector.setWindowTitle(
-            _translate("DockWidget_MassTabSelector", "MassTab Selector", None))
-        self.pushButton_ChangeList.setText(
-            _translate("DockWidget_MassTabSelector", "Change List", None))
+        DockWidget_MassTabSelector.setWindowTitle(_translate("DockWidget_MassTabSelector", "MassTab Selector", None))
+        self.pushButton_ChangeList.setText(_translate("DockWidget_MassTabSelector", "Change List", None))
+
